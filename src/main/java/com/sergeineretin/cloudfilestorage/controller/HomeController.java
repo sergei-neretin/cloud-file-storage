4dbb8e9bb66929f9b83e7367c41bd2b928f8cef9
Sergei Neretin <sergei.neretin.2001@gmail.com>
2024-09-23 13:02:49 +0700

    refactor: get method is refactored

--- src/main/java/com/sergeineretin/cloudfilestorage/controller/HomeController.java
+++ src/main/java/com/sergeineretin/cloudfilestorage/controller/HomeController.java
@@ -4,10 +4,15 @@ import com.sergeineretin.cloudfilestorage.exception.StorageException;
 import com.sergeineretin.cloudfilestorage.service.HomeService;
 import com.sergeineretin.cloudfilestorage.util.StorageUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Optional;
 
 
 @Controller
@@ -18,18 +23,30 @@ public class HomeController {
     public HomeController(HomeService homeService) {
         this.homeService = homeService;
     }
+
     @GetMapping("/home")
-    public String homePage(@RequestParam(value = "path", required = false) String path, Model model) {
-        String fullPath = StorageUtils.getFullDirectoryPath(path);
-        if (homeService.isObjectExist(fullPath)) {
-            model.addAttribute("items", homeService.getList(fullPath));
-            model.addAttribute("hierarchy",  StorageUtils.getNavigationHierarchy(path));
-            model.addAttribute("path", path);
+    public String homePage(@RequestParam(value = "path", required = false) Optional<String> optionalPath, Model model) {
+        if(!isAuthenticated()) {
             return "home";
-        } else {
-            model.addAttribute("message", "folder does not exist");
+        }
+        String path = optionalPath.orElse("");
+        String fullPath = StorageUtils.getFullDirectoryPath(path);
+        if (!homeService.isObjectExist(fullPath)) {
+            model.addAttribute("message", "Folder does not exist");
             return "error";
         }
+        model.addAttribute("items", homeService.getList(fullPath));
+        model.addAttribute("hierarchy", StorageUtils.getNavigationHierarchy(path));
+        model.addAttribute("path", path);
+
+        return "home";
+    }
+
+    private boolean isAuthenticated() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication != null
+                && authentication.isAuthenticated()
+                && !(authentication instanceof AnonymousAuthenticationToken);
     }
 
     @PostMapping("/home/new-file")
