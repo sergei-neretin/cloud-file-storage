22538a26e4e78b9f9a3ba7fd98882e5c2b4d6dec
Sergei Neretin <sergei.neretin.2001@gmail.com>
2024-09-23 13:59:03 +0700

    fix: creating user-{id}-files root folder during registration

--- src/main/java/com/sergeineretin/cloudfilestorage/service/RegistrationService.java
+++ src/main/java/com/sergeineretin/cloudfilestorage/service/RegistrationService.java
@@ -1,30 +1,44 @@
 package com.sergeineretin.cloudfilestorage.service;
 
 import com.sergeineretin.cloudfilestorage.dto.UserDto;
+import com.sergeineretin.cloudfilestorage.exception.StorageException;
 import com.sergeineretin.cloudfilestorage.exception.UserAlreadyExistException;
 import com.sergeineretin.cloudfilestorage.model.User;
 import com.sergeineretin.cloudfilestorage.repository.UserRepository;
 import com.sergeineretin.cloudfilestorage.util.Roles;
+import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.ByteArrayInputStream;
 import java.util.Optional;
+
+import static com.sergeineretin.cloudfilestorage.util.StorageUtils.USER_FILES_BUCKET_NAME;
 
 @Service
 public class RegistrationService {
+    private static final Logger log = LoggerFactory.getLogger(HomeService.class);
+
+
     private final UserRepository repository;
     private final ModelMapper modelMapper;
     private final PasswordEncoder passwordEncoder;
+    private final MinioClient minioClient;
     @Autowired
     public RegistrationService(ModelMapper modelMapper,
                                UserRepository repository,
+                               MinioClient minioClient,
                                PasswordEncoder passwordEncoder) {
         this.repository = repository;
         this.modelMapper = modelMapper;
         this.passwordEncoder = passwordEncoder;
+        this.minioClient = minioClient;
     }
 
     public Optional<UserDto> getUserByLogin(String login) {
@@ -39,8 +53,23 @@ public class RegistrationService {
             userDto.setRole(Roles.ROLE_USER);
             User user = modelMapper.map(userDto, User.class);
             repository.save(user);
+            createFolder(user.getId());
         } else {
             throw new UserAlreadyExistException("User already exists.");
+        }
+    }
+
+    private void createFolder(Long id) {
+        try {
+            minioClient.putObject(
+                    PutObjectArgs.builder()
+                            .bucket(USER_FILES_BUCKET_NAME)
+                            .object("user-" + id + "-files/")
+                            .stream(new ByteArrayInputStream(new byte[] {}), 0, -1)
+                            .build());
+        } catch (Exception e) {
+            log.error("Error creating folder '{}'", "user-" + id + "-files/", e);
+            throw new StorageException("Failed to store folder 'user-" + id + "-files/'", e);
         }
     }
 }
