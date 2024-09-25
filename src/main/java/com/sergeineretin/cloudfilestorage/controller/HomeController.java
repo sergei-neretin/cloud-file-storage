ee089d5d5d6ca3050a46dea2b592a445796126db
Sergei Neretin <sergei.neretin.2001@gmail.com>
2024-09-25 23:34:20 +0700

    feat: added download method to HomeController

--- src/main/java/com/sergeineretin/cloudfilestorage/controller/HomeController.java
+++ src/main/java/com/sergeineretin/cloudfilestorage/controller/HomeController.java
@@ -1,9 +1,15 @@
 package com.sergeineretin.cloudfilestorage.controller;
 
+import com.sergeineretin.cloudfilestorage.dto.FileDownloadRequest;
+import com.sergeineretin.cloudfilestorage.exception.FileDownloadException;
 import com.sergeineretin.cloudfilestorage.exception.StorageException;
 import com.sergeineretin.cloudfilestorage.service.HomeService;
 import com.sergeineretin.cloudfilestorage.util.StorageUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -12,6 +18,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.ConstraintViolationException;
 import java.util.Optional;
 
 
@@ -52,7 +59,7 @@ public class HomeController {
     @PostMapping("/home/new-file")
     public String submit(@RequestParam(value = "path", required = false) String path,
                          @RequestParam("file") MultipartFile file) {
-        homeService.createFile(StorageUtils.getFullDirectoryPath(path), file);
+        homeService.uploadFile(StorageUtils.getFullDirectoryPath(path), file);
         return  redirectToHomePage(path);
     }
 
@@ -63,13 +70,15 @@ public class HomeController {
         return redirectToHomePage(path);
     }
 
-    @PostMapping("/home/delete") String deletePath(@RequestParam(value = "path") String path,
+    @PostMapping("/home/delete")
+    String deletePath(@RequestParam(value = "path") String path,
                                                      @RequestParam(value = "objectName") String name) {
         homeService.delete( StorageUtils.getFullDirectoryPath(path) + name);
         return redirectToHomePage(path);
     }
 
-    @PostMapping("/home/rename") String rename(@RequestParam(value = "path", required = false) String path,
+    @PostMapping("/home/rename")
+    String rename(@RequestParam(value = "path", required = false) String path,
                                                @RequestParam(value = "name") String name,
                                                @RequestParam(value = "newName") String newName) {
         homeService.rename( StorageUtils.getFullDirectoryPath(path) + name,
@@ -77,19 +86,43 @@ public class HomeController {
         return redirectToHomePage(path);
     }
 
+    @GetMapping(value = "/home/download",
+            produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public ResponseEntity<ByteArrayResource> download(@ModelAttribute("fileDownloadRequest") FileDownloadRequest fileDownloadRequest) {
+        ByteArrayResource byteArrayResource = homeService.downloadFile(StorageUtils.getFullDirectoryPath( fileDownloadRequest.getPath()) + fileDownloadRequest.getName());
+        return ResponseEntity.ok().contentType(MediaType.parseMediaType("application/octet-stream"))
+                .contentLength(byteArrayResource.contentLength())
+                .header("Content-Disposition", "attachment; filename=\"" + fileDownloadRequest.getName() + "\"")
+                .body(byteArrayResource);
+    }
+
     @GetMapping("/admin")
     public String adminPage() {
         return "admin";
     }
 
-    @ExceptionHandler
+    private String redirectToHomePage(String path) {
+        return "redirect:/home?path=" + (path != null && !path.isEmpty() ? path : "");
+    }
+
+    @ExceptionHandler(StorageException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     private String handleStorageException(StorageException ex, Model model) {
         model.addAttribute("message", ex.getMessage());
         return "error";
     }
 
-    private String redirectToHomePage(String path) {
-        return "redirect:/home?path=" + (path != null ? path : "");
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleViolationException(ConstraintViolationException ex, Model model) {
+        model.addAttribute("message", ex.getMessage());
+        return "error";
     }
 
+    @ExceptionHandler(FileDownloadException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleFileDownloadException(FileDownloadException ex, Model model) {
+        model.addAttribute("message", ex.getMessage());
+        return "error";
+    }
 }
---
+++ src/main/java/com/sergeineretin/cloudfilestorage/exception/FileDownloadException.java
@@ -0,0 +1,7 @@
+package com.sergeineretin.cloudfilestorage.exception;
+
+public class FileDownloadException extends RuntimeException {
+    public FileDownloadException(String message) {
+        super(message);
+    }
+}
