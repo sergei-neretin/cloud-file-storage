bdf82562f05499b65e4da567b7bd0585a133ec09
Sergei Neretin <sergei.neretin.2001@gmail.com>
2024-09-29 18:26:25 +0700

    refactor: Modify SpringConfig to load beans with hardcoded values for development profile.

--- src/main/java/com/sergeineretin/cloudfilestorage/config/SpringConfig.java
+++ src/main/java/com/sergeineretin/cloudfilestorage/config/SpringConfig.java
@@ -2,8 +2,10 @@ package com.sergeineretin.cloudfilestorage.config;
 
 import io.minio.MinioClient;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class SpringConfig {
@@ -14,10 +16,21 @@ public class SpringConfig {
     }
 
     @Bean
-    MinioClient minioClient() {
+    MinioClient minioClient(@Value("${MINIO_ROOT_USER}") String accessKey,
+                            @Value("${MINIO_ROOT_PASSWORD}") String secretKey) {
+        return MinioClient.builder()
+                .endpoint("http://127.0.0.1:9000")
+                .credentials(accessKey, secretKey)
+                .build();
+    }
+
+    @Bean
+    @Profile("dev")
+    MinioClient minioClientDev() {
         return MinioClient.builder()
                 .endpoint("http://127.0.0.1:9000")
                 .credentials("username", "password")
                 .build();
     }
+
 }
